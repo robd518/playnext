1a789b4802ac356b94635969a95b1b1c157b15af
Rob D'Aveta <rob.daveta@gmail.com>
2020-08-14 10:26:57 -0400

    basic form skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,30 @@
 import React from 'react'
 import { Form, Input, SubmitButton } from '../components/FormElements'
-import { SteamSignIn } from '../components/SteamLogin'
 import useSearchForm from '../hooks/useSearchForm'
 
 const Index = () => {
 
-    const { values, 
+    const { values,
         handleOnSubmit,
         handleOnChange
     } = useSearchForm()
 
     return (
-        <>
-            <SteamSignIn />
-            <Form
-                name="searchForm"
-                title="Play Next"
-                onSubmit={handleOnSubmit}
-            >
-                <Input
-                    name="searchInput"
-                    value={values.searchInput || ''}
-                    onChange={handleOnChange}
-                />
-                <SubmitButton
-                    name={"formSubmit"}
-                    text={"Search"}
-                />
-            </Form>
-        </>
+        <Form
+            name="searchForm"
+            title="Play Next"
+            onSubmit={handleOnSubmit}
+        >
+            <Input
+                name="searchInput"
+                value={values.searchInput || ''}
+                onChange={handleOnChange}
+            />
+            <SubmitButton
+                name={"formSubmit"}
+                text={"Search"}
+            />
+        </Form>
     )
 }
 
